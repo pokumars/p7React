446604e8513a1ae09cd09f525ddca97723c0ecd7
oheneba <ohenebakob@hotmail.com>
2019-12-10 19:45:02 +0200

    make react app from scratch with webpack not npx create-react-app

---
+++ p7webpack/src/index.js
@@ -0,0 +1,11 @@
+import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/*
+const hello = name => {
+  console.log(`hello ${name}`)
+}
+
+App()*/
+ReactDOM.render(<App />, document.getElementById('root'))
